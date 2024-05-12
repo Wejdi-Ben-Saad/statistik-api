3cc1cab4f8479d02e6b3156a555dc426f2dbee91
Wejdi-Ben-Saad <bensaad.wejdi@gmail.com>
2024-05-12 16:50:14 +0200

    include database connection settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -81,14 +81,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        # 'HOST': os.environ.get('DB_HOST'),
-        # 'NAME': os.environ.get('DB_NAME'),
-        # 'USER': os.environ.get('DB_USER'),
-        # 'PASSWORD': os.environ.get('DB_PASS'),
-        'HOST': 'localhost',
-        'NAME': 'statistik_local',
-        'USER': 'postgres',
-        'PASSWORD': 'test@postgres',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
